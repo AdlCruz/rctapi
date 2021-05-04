513f78460a76001179550e238ba6ba69404d3ea7
Arturo de la Cruz <arturodlcl@gmail.com>
2021-05-04 19:10:50 +0100

    Removed Rank column duplicates

--- R/get_study_fields.R
+++ R/get_study_fields.R
@@ -2,15 +2,18 @@
 #'
 #' This function takes a search expression and character vector with any number
 #' of field names and returns one dataframe with the merged and parsed csv API response.
-#' @param search_expr A string following the querying guidelines on clinicaltrials.gov
-#' @param fields A character vector with any field names found on clinicaltrials.gov
-#' @keywords A
+#' @param search_expr A string following the querying guidelines on clinicaltrials.gov.
+#' @param fields A character vector with any field names found on clinicaltrials.gov.
+#' @param max_studies A number indicating how many studies of all the ones that 
+#' match the search expression will be returned.
+#' @keywords get many fields
 #' @export
 #' @examples
 #' get_study_fields(expr = 'heart attack',fields = basic_info_and_results_fields())
 
-get_study_fields <- function(search_expr, fields, # for all fields use study_fields_vector()
-                                                  # or all_fields_vector
+get_study_fields <- function(search_expr, 
+                             fields, # for all fields use study_fields_vector() 
+                                     # or all_fields_vector
                              max_studies = 30) {
   fields_matrix <- split_vector_unique(fields,20)
 
@@ -26,8 +29,10 @@ get_study_fields <- function(search_expr, fields, # for all fields use study_fie
     else {
       next_df <- get_fields(search_expr, fields = fields_list, max_studies = max_studies)
       df_merge <- cbind(df_merge,next_df,stringsAsFactors = FALSE)
+      
     }
   }
+  df_merge <- df_merge[, !duplicated(colnames(df_merge))]
   return(df_merge)
 }
 
