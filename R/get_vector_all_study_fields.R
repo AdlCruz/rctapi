d3a757450050eb2c4dc62849a04829c04114bca0
Arturo de la Cruz <arturodlcl@gmail.com>
2021-06-29 13:06:58 +0100

    workaround all fields vector

--- R/get_vector_all_study_fields.R
+++ R/get_vector_all_study_fields.R
@@ -11,8 +11,11 @@
 get_vector_all_study_fields <- function() {
 
   # returns study fields character vector
-  fields_list <- json_handler(glue::glue("{b_url}{b_info}study_fields_list?{fmt_json}"))
-  fields_character <- (fields_list$Fields)
+  dec_content <- jsonlite::fromJSON(httr::content(request_ct(glue::glue("{b_url}{b_info}study_fields_list?{fmt_json}")), "text"), simplifyVector = TRUE)
+  
+  decoded_response <- dec_content[[1]]
+  
+  fields_character <- (decoded_response$Fields)
 
   return(fields_character)
 }
